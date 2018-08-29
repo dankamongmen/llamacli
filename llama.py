ce9dd38974cecc8ac8e854eea87627e2e8f04f29
nick black <dankamongmen@gmail.com>
2018-08-29 12:52:10 -0400

    subclass from Cmd

--- llama.py
+++ llama.py
@@ -14,16 +14,23 @@ except KeyError:
     print("please define environment variables LLAMA and PASSWORD")
     sys.exit(1)
 
-ui = cmd.Cmd()
-
 llama = str.lower(payload['username'])
 
 print(sys.argv)
 print(llama)
 
-with requests.Session() as s:
-    p = s.post('https://www.learnedleague.com/ucp.php?mode=login', data=payload)
-    # FIXME how to verify that we successfully logged in?
-    r = s.get('https://learnedleague.com/profiles.php?' + llama)
-    print(r.text)
-    ui.cmdloop()
+class LLamaShell(cmd.Cmd):
+    intro = 'LLama, taboot, taboot! "help" lists commands.\n'
+    prompt = '[llcli] '
+    def do_today(self, arg):
+        print('go grab today\'s data')
+    def do_exit(self, arg):
+        return True
+
+if __name__ == '__main__':
+    with requests.Session() as s:
+        p = s.post('https://www.learnedleague.com/ucp.php?mode=login', data=payload)
+        # FIXME how to verify that we successfully logged in?
+        r = s.get('https://learnedleague.com/profiles.php?' + llama)
+        print(r.text)
+        LLamaShell().cmdloop()
