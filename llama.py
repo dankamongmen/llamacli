d31862045ad31adf09672b023f841b4b620cc821
nick black <dankamongmen@gmail.com>
2018-08-29 12:58:42 -0400

    use username in prompt

--- llama.py
+++ llama.py
@@ -14,20 +14,16 @@ except KeyError:
     print("please define environment variables LLAMA and PASSWORD")
     sys.exit(1)
 
-llama = str.lower(payload['username'])
-
-print(sys.argv)
-print(llama)
-
 class LLamaShell(cmd.Cmd):
     intro = 'LLama, taboot, taboot! "help" lists commands.\n'
-    prompt = '[llcli] '
+    prompt = '[' + payload['username'] + '] '
     def do_today(self, arg):
         print('go grab today\'s data')
     def do_exit(self, arg):
         return True
 
 if __name__ == '__main__':
+    llama = str.lower(payload['username'])
     with requests.Session() as s:
         p = s.post('https://www.learnedleague.com/ucp.php?mode=login', data=payload)
         # FIXME how to verify that we successfully logged in?
